5f3de637d84314b27e82b686a54e5a2a6b55e25d
AlexNaupay <alexnaupay@gmail.com>
2016-02-04 18:10:00 -0500

    m01 - spring bean

--- src/main/java/com/sistemas/beans/Hello.java
+++ src/main/java/com/sistemas/beans/Hello.java
@@ -1,13 +1,6 @@
 package com.sistemas.beans;
 
 public class Hello {
-	private String firstName;
-	private String lastName;
-	private String schoolName;
-	private String gener;
-	private int age;
-		
-	private String city;
-	private String bankName;
+
 
 }
--- src/main/java/com/sistemas/beans/SpringBean.java
+++ src/main/java/com/sistemas/beans/SpringBean.java
@@ -1,5 +1,12 @@
 package com.sistemas.beans;
 
 public class SpringBean {
+    private String firstName;
+    private String lastName;
+    private String schoolName;
+    private String gener;
+    private int age;
 
+    private String city;
+    private String bankName;
 }
