02a7358c4ba534e655937ce075f432edc64969ae
AlexNaupay <alexnaupay@gmail.com>
2016-02-04 18:40:48 -0500

    iss1-2 - change array declaration

--- src/main/java/com/sistemas/beans/Hello.java
+++ src/main/java/com/sistemas/beans/Hello.java
@@ -1,7 +1,7 @@
 package com.sistemas.beans;
 
 public class Hello {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("Main ..");
     }
 
