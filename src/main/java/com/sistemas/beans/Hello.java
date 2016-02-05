7d0299ba1a87ac273ad2a07d03b27d2141d5ebd9
AlexNaupay <alexnaupay@gmail.com>
2016-02-04 19:51:14 -0500

    f04 - main delete sout main

--- src/main/java/com/sistemas/beans/Hello.java
+++ src/main/java/com/sistemas/beans/Hello.java
@@ -2,7 +2,6 @@ package com.sistemas.beans;
 
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Main ..");
         System.out.println("feature 1 ..");
     }
 
