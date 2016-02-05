a45fe88189534753998d94d10fd869a31f41b37a
AlexNaupay <alexnaupay@gmail.com>
2016-02-04 19:13:49 -0500

    f03 - main sout feature 1

--- src/main/java/com/sistemas/beans/Hello.java
+++ src/main/java/com/sistemas/beans/Hello.java
@@ -3,6 +3,7 @@ package com.sistemas.beans;
 public class Hello {
     public static void main(String[] args){
         System.out.println("Main ..");
+        System.out.println("feature 1 ..");
     }
 
 }
