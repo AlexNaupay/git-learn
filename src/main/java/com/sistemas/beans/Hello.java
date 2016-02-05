ce1f6dbc391ef9ec992c4c00eb72ed47bfdbbe20
AlexNaupay <alexnaupay@gmail.com>
2016-02-05 01:58:11 -0500

    c1 - e1 add sout exp 1

--- src/main/java/com/sistemas/beans/Hello.java
+++ src/main/java/com/sistemas/beans/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Main ..");
         System.out.println("feature 1 ..");
+        System.out.println("Experimental 1");
     }
 
 }
