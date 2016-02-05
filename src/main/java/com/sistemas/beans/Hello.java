3da4865b2665e004a52cabf0ff8ecb5e5a975a4d
AlexNaupay <alexnaupay@gmail.com>
2016-02-05 11:27:24 -0500

    c6 - m3 delete exp 1

--- src/main/java/com/sistemas/beans/Hello.java
+++ src/main/java/com/sistemas/beans/Hello.java
@@ -4,7 +4,6 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Main ..");
         System.out.println("feature 1 ..");
-        System.out.println("Experimental 1");
     }
 
 }
